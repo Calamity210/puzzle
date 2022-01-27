7380083357e7cabee26665c1c8f26b96de2e63c4
Calamity210 <aayanraja210@gmail.com>
2022-01-27 12:09:34 -0500

    Separate Dash Particle Image

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,9 @@ import 'package:bonfire/bonfire.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:image/image.dart' as img;
 import 'package:puzzle/background/background_painter.dart';
 import 'package:puzzle/game/game_page.dart';
-import 'package:puzzle/particles/particle_circles.dart';
-
-late ByteData imageBytes;
+import 'package:puzzle/particles/dash_particles.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,7 +15,6 @@ Future<void> main() async {
     await Flame.device.fullScreen();
   }
 
-  imageBytes = await rootBundle.load('assets/images/dash/dash.png');
   await FlameAudio.audioCache.loadAll(['sfx/click.mp3']);
 
   async.unawaited(
@@ -64,111 +59,48 @@ Future<void> main() async {
     ]),
   );
 
-  runApp(
-    LayoutBuilder(
-      builder: (context, c) {
-        return MaterialApp(
-          home: HomePage(imageSize: (c.maxHeight * 0.75).toInt()),
-        );
-      },
-    ),
-  );
+  runApp(const MaterialApp(home: HomePage()));
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({required this.imageSize, Key? key}) : super(key: key);
-
-  final int imageSize;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  async.Timer? _timer;
-
-  var _mouseX = 0.0;
-  var _mouseY = 0.0;
-
-  late final image = img.copyResize(
-    img.decodeImage(
-      imageBytes.buffer.asUint8List().buffer.asUint8List(),
-    )!,
-    height: widget.imageSize,
-    width: widget.imageSize,
-  );
-
-  late ImageParticles ip = ImageParticles(
-    image,
-    (widget.imageSize * 0.03).toInt(),
-  );
-
-  @override
-  void initState() {
-    super.initState();
-    _timer = async.Timer.periodic(const Duration(milliseconds: 34), (timer) {
-      // setState(() {
-      //   repulsionChangeDistance = max(0, repulsionChangeDistance - 1.5);
-      // });
-    });
-  }
-
-  @override
-  void dispose() {
-    _timer?.cancel();
-    _timer = null;
-
-    super.dispose();
-  }
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF061547),
-      child: CustomPaint(
-        painter: BackgroundPainter(),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            TextButton(
-              onPressed: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const GamePage(),
-                  ),
-                );
-              },
-              child: const Text(
-                'Start',
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            CustomPaint(
-              isComplex: true,
-              willChange: true,
-              painter: ImageParticlesPainter(ip, _mouseX, _mouseY),
-              child: SizedBox(
-                height: widget.imageSize.toDouble(),
-                width: widget.imageSize.toDouble(),
-                child: GestureDetector(
-                  excludeFromSemantics: true,
-                  onPanUpdate: (details) {
-                    repulsionChangeDistance = 150;
-                    _mouseX = details.localPosition.dx;
-                    _mouseY = details.localPosition.dy;
+    return LayoutBuilder(
+      builder: (context, c) {
+        print('layout');
+        return Container(
+          color: const Color(0xFF061547),
+          child: CustomPaint(
+            painter: BackgroundPainter(),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextButton(
+                  onPressed: () async {
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const GamePage(),
+                      ),
+                    );
                   },
+                  child: const Text(
+                    'Start',
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.blue,
+                    ),
+                  ),
                 ),
-              ),
+                DashParticles(imageSize: (c.maxHeight * 0.75).toInt()),
+              ],
             ),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
     );
   }
 }
---
+++ lib/particles/dash_particles.dart
@@ -0,0 +1,84 @@
+import 'dart:async' as async;
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image/image.dart' as img;
+import 'package:puzzle/particles/particle_circles.dart';
+
+class DashParticles extends StatefulWidget {
+  const DashParticles({required this.imageSize, Key? key}) : super(key: key);
+
+  final int imageSize;
+
+  @override
+  _DashParticlesState createState() => _DashParticlesState();
+}
+
+class _DashParticlesState extends State<DashParticles> {
+  late final Future<ImageParticles> getIP = getImageParticles();
+  async.Timer? _timer;
+
+  var _mouseX = 0.0;
+  var _mouseY = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = async.Timer.periodic(const Duration(milliseconds: 34), (timer) {
+      setState(() {
+        repulsionChangeDistance = max(0, repulsionChangeDistance - 1.5);
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    _timer = null;
+
+    super.dispose();
+  }
+
+  Future<ImageParticles> getImageParticles() async {
+    final dashImageBytes = await rootBundle.load('assets/images/dash/dash.png');
+    return ImageParticles(
+      img.copyResize(
+        img.decodeImage(dashImageBytes.buffer.asUint8List())!,
+        height: widget.imageSize,
+        width: widget.imageSize,
+      ),
+      (widget.imageSize * 0.03).toInt(),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<ImageParticles>(
+      future: getIP,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return CustomPaint(
+            isComplex: true,
+            willChange: true,
+            painter: ImageParticlesPainter(snapshot.data!, _mouseX, _mouseY),
+            child: SizedBox(
+              height: widget.imageSize.toDouble(),
+              width: widget.imageSize.toDouble(),
+              child: GestureDetector(
+                excludeFromSemantics: true,
+                onPanUpdate: (details) {
+                  repulsionChangeDistance = 150;
+                  _mouseX = details.localPosition.dx;
+                  _mouseY = details.localPosition.dy;
+                },
+              ),
+            ),
+          );
+        }
+
+        return const CircularProgressIndicator();
+      },
+    );
+  }
+}
